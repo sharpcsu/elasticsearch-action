0e55a64e50a2e92ec7f5e09f963a16c3e315da41
Sharp <1697708997@qq.com>
2019-01-02 15:52:42 +0800

    Initial commit

---
+++ src/test/java/com/csu/es/ElasticsearchActionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.csu.es;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchActionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
